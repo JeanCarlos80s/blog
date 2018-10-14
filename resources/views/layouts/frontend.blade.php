719cea95cdd65f717343679ab674d8dc95e12dee
jeancarlos80s <jeancarlos80s@gmail.com>
2018-10-14 17:05:56 -0300

    Adicionando AddThis para adicionar botões de redes sociais ao blog

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -155,6 +155,8 @@
 <script src="{{ asset('app/js/ScrollMagic.min.js') }}"></script>
 <script src="{{ asset('app/js/animation.velocity.min.js') }}"></script>
 
+<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5bc39f0b90744e6f"></script>
 
 <!-- ...end JS Script -->
 
